ece70bc93ec1acb1477e856a6f8be4757933b461
prateeksahu-in <prateeksahu0101@gmail.com>
2022-06-26 20:32:45 +0530

    Header completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
 
 const Header = () => {
   return (
@@ -12,11 +14,14 @@ const Header = () => {
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA/>
 
+        <HeaderSocials/>
+
         <div className="me">
           <img src={ME} alt="" />
         </div>
 
         <a href="#contact" className='scroll__down'>Scroll Down</a>
+      
       </div>
     </header>
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {BsInstagram} from 'react-icons/bs'
+import './header.css'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com" target="_blank"></a>
-        <a href="https://github.com" target="_blank"></a>
-        <a href="https://instagram.com" target="_blank"></a>
+        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com" target="_blank"><FaGithub/></a>
+        <a href="https://instagram.com" target="_blank"><BsInstagram/></a>
     </div>
   )
 }
