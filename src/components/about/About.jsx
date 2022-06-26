8aba255d61c4ad485f894a94c2a2599a0fc9b0b7
prateeksahu-in <prateeksahu0101@gmail.com>
2022-06-27 01:04:35 +0530

    added about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.png'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
@@ -8,11 +11,36 @@ const About = () => {
       <h5>Get To Know</h5>
       <h2>About Me</h2>
       <div className="container about__container">
-        <div className="aboutme">  
+        <div className="about__me">  
           <div className="about__me-image">
-            <img src={ME} alt="" />
+            <img src={ME} alt="About Image" />
           </div>
         </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+                <FaAward className='about_icon'/>              
+                <h5>Experience</h5>
+                <small>1+ Years Working</small>
+            </article>
+            <article className='about__card'>
+                <FiUsers className='about_icon'/>              
+                <h5>Clients</h5>
+                <small>10+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+                <VscFolderLibrary className='about_icon'/>              
+                <h5>Projects</h5>
+                <small>5+ Completed</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur ratione, saepe earum rem odio cum maxime non magni reiciendis et error nobis quia nisi cupiditate, corporis magnam voluptatibus dolorum qui.</p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+
       </div>
     </section>
   )
