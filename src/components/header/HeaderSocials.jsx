0298a186fb1aa1c42c900846bde5637ab2569710
prateeksahu-in <prateeksahu0101@gmail.com>
2022-07-14 00:14:00 +0530

    profolio ui complited

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,9 +7,9 @@ import './header.css'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
-        <a href="https://instagram.com" target="_blank"><BsInstagram/></a>
+        <a href="https://www.linkedin.com/in/prateek-sahu-796993145/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/prateeksahu-in" target="_blank"><FaGithub/></a>
+        <a href="https://www.instagram.com/prateeksahu.me/" target="_blank"><BsInstagram/></a>
     </div>
   )
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -70,6 +70,14 @@ const Services = () => {
             <BiCheck className="service__list-icon"/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </li>
+            <li>  
+            <BiCheck className="service__list-icon"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>  
+            <BiCheck className="service__list-icon"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
           </ul>
         </article>
         {/* END OF WEBDEVELOPMENT */}
